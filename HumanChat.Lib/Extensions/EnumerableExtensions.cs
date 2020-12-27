61fddee894e64f47c2aa0b5e026c6e87d3bab4b8
Boris Vashov <vashovway@gmail.com>
2020-12-27 23:48:30 +0300

    init

---
+++ HumanChat.Lib/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace HumanChat.Lib.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> values)
+        {
+            return values == null || !values.Any();
+        }
+
+        /// <summary>
+        /// Check that values is not null and has any element.
+        /// </summary>
+        public static bool HasAny<T>(this IEnumerable<T> values)
+        {
+            return !values.IsNullOrEmpty();
+        }
+    }
+}
